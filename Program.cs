d73403344c3c3da173a48572a887d7d8bd04f0d4
Abigail <131455685+ST10045251@users.noreply.github.com>
2024-10-01 23:58:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{action=Index}/{id?}",
+        defaults: new { controller = "Storage" }
+    );
+});
+
+app.Run();
